77ee4b322c4fa1b837f2b1698a596497319756aa
rolex <zengronghui@chinadaas.com>
2019-11-04 13:58:09 +0800

    deadlock

---
+++ multi-thread/src/main/java/com/rolex/microlabs/lock/DeadlockDemo.java
@@ -0,0 +1,60 @@
+package com.rolex.microlabs.lock;
+
+/*******************************************************************************
+ * - Copyright (c)  2018  chinadaas.com
+ *   @author rolex
+ * - File Name: com.rolex.Deadlock
+ * - Description:
+ *
+ *
+ * - Function List:
+ *
+ *
+ * - History:
+ * Date         Author          Modification
+ * 2019/02/19   rolex           Create file
+ *******************************************************************************/
+public class DeadlockDemo {
+    
+    Object lock1 = new Object();
+    Object lock2 = new Object();
+    
+    public void m1() {
+        synchronized (lock1) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            m2();
+        }
+    }
+    
+    public void m2() {
+        synchronized (lock2) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            m1();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                new DeadlockDemo().m1();
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                new DeadlockDemo().m2();
+            }
+        }).start();
+        System.out.println("finished");
+    }
+    
+}
